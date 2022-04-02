d16b739b6c509f8746d191ecbc4411f7db1f8a4e
GuntisKalnins <guntis.kalnins92@gmail.com>
2022-04-02 16:34:46 +0300

    "To top" button added

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,3 +1,8 @@
+if(window.innerWidth >= 768 && window.innerWidth <= 1200)
+{
+    document.getElementById('link-text-1').innerHTML = "VĒLIES PALĪDZĒT KĀDĀM PIEPILDĪT SAPNI?";
+    document.getElementById('link-text-2').innerHTML = "VĒLIES PIEPILDĪT SAVU SAPNI?";
+}
 window.onresize = function() {
     if(window.innerWidth >= 768 && window.innerWidth <= 1200)
     {
@@ -5,3 +10,9 @@ window.onresize = function() {
         document.getElementById('link-text-2').innerHTML = "VĒLIES PIEPILDĪT SAVU SAPNI?";
     }
 }
+window.addEventListener('scroll', function() {
+	let element = document.getElementById('to-top');
+    let wrap = document.getElementById('wrapper');
+	element.style["display"] = "inline";
+    wrap.style["margin-top"] = "-60px";
+});
