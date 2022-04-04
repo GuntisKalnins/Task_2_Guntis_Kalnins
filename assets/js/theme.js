0fb58df96f5071e6fedd41390990cb65999509e2
GuntisKalnins <guntis.kalnins92@gmail.com>
2022-04-05 00:39:06 +0300

    Menu added

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,12 +1,17 @@
-window.addEventListener('scroll', function() {
+/*window.addEventListener('scroll', function() {
 	let element = document.getElementById('to-top');
     let wrap = document.getElementById('wrapper');
 	element.style["display"] = "inline";
     wrap.style["margin-top"] = "-60px";
 });
-
+*/
 if(window.innerWidth >= 768 && window.innerWidth <= 1200)
 {
     document.getElementById('link-text-1').innerHTML = "VĒLIES PALĪDZĒT KĀDĀM PIEPILDĪT SAPNI?";
     document.getElementById('link-text-2').innerHTML = "VĒLIES PIEPILDĪT SAVU SAPNI?";
 }
+
+$(function(){
+    $('.selectpicker').selectpicker();
+    
+});
